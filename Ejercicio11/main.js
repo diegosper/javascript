8237a888aaa86f1da9df50dad7a31c5c652bd2c1
Diego Sperber <diego.sperber@gmail.com>
2022-08-11 13:54:10 +0200

    Ejercicio 11

---
+++ Ejercicio11/main.js
@@ -0,0 +1,14 @@
+//bucle for
+console.log("Bucle for: ");
+for (let i = 0; i < 10; i++) {
+    console.log("I ❤️ code");
+    
+}
+
+//bucle while
+console.log("\n" + "Bucle while: ");
+let i = 0;
+while (i < 10){
+    console.log("I ❤️ code");
+    i++;
+}
